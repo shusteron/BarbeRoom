fd90f22d8d50ada4bae8d405305821f89428fb25
Yuval10Dahan <dahan.yuval@gmail.com>
2024-01-17 19:16:32 +0200

    commit

---
+++ app/barbers/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>what needs to be: signup and login for barbers</div>
+  )
+}
+
+export default page
---
+++ app/clients/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>what needs to be: signup and login for clients</div>
+  )
+}
+
+export default page

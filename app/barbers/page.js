c6ed213c884f5bd09e3a96855cbeeeb586aaf595
Yuval10Dahan <dahan.yuval@gmail.com>
2024-01-17 19:05:32 +0200

    delete

--- app/barbers/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>what needs to be: signup and login for barbers</div>
-  )
-}
-
-export default page
--- app/clients/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>what needs to be: signup and login for clients</div>
-  )
-}
-
-export default page

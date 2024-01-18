681cb408462f493e680c7211b6d403b07690d0c2
Yuval10Dahan <dahan.yuval@gmail.com>
2024-01-18 12:47:26 +0200

    commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,13 +4,13 @@ import Image from 'next/image'
 import Link from "next/link"
 import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from "next-auth/react"
-
+import '@styles/globals.css'
 
 
 
 const Nav = () => {
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
+    <nav className='.navbar'>
       <Link href='/' className='flex gap-2 flex-center'>
         <Image
           src="/images/Logo.svg" 

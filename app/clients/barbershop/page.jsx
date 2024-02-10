47c873f43a520f0ed39d5e93bd4192c7cc3eef8c
Yuval10Dahan <dahan.yuval@gmail.com>
2024-02-10 23:59:35 +0200

    #40 changing fonts and styles all over

--- app/clients/barbershop/page.jsx
+++ app/clients/barbershop/page.jsx
@@ -1,13 +1,31 @@
 "use client"
+
+
 import Link from "next/link"
-// import '@styles/globals.css'
-import "../../../styles/globals.css"
-
-
+import Image from "next/image"
+import '../../../styles/globals.css'
 
 const barbershopPage = () => {
   return (
-    <div className="center">barbershopPage</div>
+    <div className="container white-text center" style={{ marginBottom: '300px' }}>
+      <div className="details" style={{ marginRight: '50px', marginBottom: '300px'}}>
+        <h1 style={{ marginBottom: '20px' }}>BarbeRoom מספרת</h1>
+        <p style={{ marginBottom: '20px' }}>הנדסת תוכנה, אריאל</p>
+        <p>054-8085030</p>
+        <p>054-5605087</p>
+        <p>052-3201458</p>
+        <p>052-2860930</p>
+      </div>
+      <div className="opening-hours " style={{ marginBottom: '20px' }}>
+        <h2>:שעות פתיחה</h2>
+        <p>ראשון - שישי 21:00 - 08:00</p>
+      </div>
+      <div className="navigation-link" style={{ marginBottom: '20px' }}>
+        {/* <Link href="https://waze.com/ul?q=BarbeRoom%20Barbershop%2C%20Handasa%20100%2C%20Ariel">
+          <a target="_blank" rel="noopener noreferrer">Navigate with Waze</a>
+        </Link> */}
+      </div>
+    </div>
   )
 }
 

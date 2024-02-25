a611c2b3699af9463fd91790d690b735f3fac685
NisimAtiya <118813292+NisimAtiya@users.noreply.github.com>
2024-02-25 17:08:41 +0200

    # 60 - fix RegisterShiftPage You can only choose one shift You can't choose Mondays

--- app/barbers/shiftOrganizer/page.jsx
+++ app/barbers/shiftOrganizer/page.jsx
@@ -21,6 +21,15 @@ import 'react-datepicker/dist/react-datepicker.css';
 
 
 const RegisterShiftPage = () => {
+  const isMonday = (date) => {
+    return date.getDay() != 1; // Monday is represented by 1 in JavaScript's getDay() function
+  };
+
+  const disableMonday = (date) => {
+    return isMonday(date);
+  };
+
+
   const [shiftDay, setShiftDay] = useState(new Date());
   const [morningShift, setMorningShift] = useState(false);
   const [eveningShift, setEveningShift] = useState(false);
@@ -87,40 +96,33 @@ const RegisterShiftPage = () => {
             className="p-2 border border-gray-300 rounded-lg focus:outline-none focus:border-gray-600 text-black"
             minDate={new Date()} // Set minimum date to the current day
             dateFormat="yyyy-MM-dd"
+            filterDate={disableMonday} // Disable Mondays
 />
         </div>
 
-        {/* Morning Shift */}
-          <div className="mb-4">
-            {formik.touched.morningShift && formik.errors.morningShift && (
-              <p className="text-red-500">{formik.errors.morningShift}</p>
-            )}
-            <label>
-              <input
-                type="checkbox"
-                checked={formik.values.morningShift}
-                onChange={(e) => formik.setFieldValue("morningShift", e.target.checked)}
-                name="morningShift"
-              />
-              Morning Shift
-            </label>
-          </div>
-
-          {/* Evening Shift */}
-          <div className="mb-4">
-            {formik.touched.eveningShift && formik.errors.eveningShift && (
-              <p className="text-red-500">{formik.errors.eveningShift}</p>
-            )}
-            <label>
-              <input
-                type="checkbox"
-                checked={formik.values.eveningShift}
-                onChange={(e) => formik.setFieldValue("eveningShift", e.target.checked)}
-                name="eveningShift"
-              />
-              Evening Shift
-            </label>
-          </div> 
+        {/* Select Shift */}
+<div className="mb-4">
+  <label>
+    <input
+      type="radio"
+      value="morning"
+      checked={formik.values.shift === 'morning'}
+      onChange={() => formik.setFieldValue('shift', 'morning')}
+    />
+    Morning Shift
+  </label>
+</div>
+<div className="mb-4">
+  <label>
+    <input
+      type="radio"
+      value="evening"
+      checked={formik.values.shift === 'evening'}
+      onChange={() => formik.setFieldValue('shift', 'evening')}
+    />
+    Evening Shift
+  </label>
+</div>
 
           <div className="flex items-center justify-center">
   <button
@@ -136,9 +138,6 @@ const RegisterShiftPage = () => {
 </div>
 
       </form>
-      <div className='absolute -z-10 w-full'>
-        <Image src={Background} alt="Background Image" className="w-full" width={1000} height={1000} />
-      </div>
     </div>
   );
 };
